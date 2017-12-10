7146210c50cb659f793a485ffa45c3b1aeebe452
fengdali <fengdali@sponia.com>
2017-12-10 09:58:40 +0800

    change indentation two

--- src/client/index.js
+++ src/client/index.js
@@ -3,18 +3,17 @@ import ReactDOM from 'react-dom';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-          <div className="app">
-            webpack-dev-hot reload simple
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className="app">
+        webpack-dev-hot reload simple
+      </div>
+    );
+  }
 }
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,38 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: {
-        app: './src/client',
-    },
-    devtool: 'inline-source-map',
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath:'/'
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        port: 9000,
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    cacheDirectory: true,
-                    presets: ['env', 'react', "stage-2"]
-                }
-            }
-        }]
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            inject:'body'
-        })
-    ],
+  entry: {
+    app: './src/client',
+  },
+  devtool: 'inline-source-map',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+            cacheDirectory: true,
+            presets: ['env', 'react', "stage-2"]
+        }
+      }
+    }]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+        template: './index.html',
+        inject: 'body'
+    })
+  ],
 };
