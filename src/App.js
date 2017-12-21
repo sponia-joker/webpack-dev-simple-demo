47a6526a92fc7b21156a2ae1febb28884783a1dc
fengdali <fengdali@sponia.com>
2017-12-21 16:15:23 +0800

    revamped the folder structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class App extends Component {
+ 
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <div className="App">App</div>
+  }
+}
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
-    app: './src/client',
+    app: './src/index.js',
   },
   devtool: 'inline-source-map',
   output: {
